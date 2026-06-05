4b663be7aa70b89dc8c116dd1d85253e00323664
Miguel <marodriguez_141@cue.edu.co>
2026-06-05 17:40:54 -0500

    Refactor del test deberiaRechazarRecargaMenorA1000

--- src/main/java/com/recargaya/service/RecargaService.java
+++ src/main/java/com/recargaya/service/RecargaService.java
@@ -6,9 +6,14 @@ import org.springframework.stereotype.Service;
 @Service
 public class RecargaService {
 
+    private static final double MIN_MONTO = 1000;
+    private static final double MAX_MONTO = 50000;
+
     public ResultadoRecarga calcularRecarga(double monto, boolean isPremium) {
-        if (monto < 1000) {
-            return new ResultadoRecarga(false, "Monto invalido: debe estar entre 1000 y 50000", 0, 0);
+        if (monto < MIN_MONTO) {
+            return new ResultadoRecarga(false,
+                "Monto invalido: debe estar entre " + (int)MIN_MONTO + " y " + (int)MAX_MONTO,
+                0, 0);
         }
 
         return new ResultadoRecarga(true, "Recarga aceptada", monto, 0);
